88e10783d3ce01092fdb552e11f4fcc0d47f4f8c
Bassem Dimassi <dimassibassem99@gmail.com>
2022-01-17 22:38:10 +0100

    two

--- index.js
+++ index.js
@@ -1,28 +1,18 @@
-const mysql = require('mysql');
+
 const express = require('express');
+const bodyParser = require('body-parser');
+var mysqlConnection = require('./db.js')
 var app = express();
-const bodyparser = require('body-parser');
 
-app.use(bodyparser.json());
-
-var mysqlConnection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'EmployeeDB',
-    multipleStatements: true
-});
-
-mysqlConnection.connect((err) => {
-    if (!err)
-        console.log('DB connection succeded.');
-    else
-        console.log('DB connection failed \n Error : ' + JSON.stringify(err, undefined, 2));
-});
-
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.set('view engine', 'pug')
 
 app.listen(3000, () => console.log('Express server is runnig at port no : 3000'));
 
+app.get('/', function (req, res) {
+    res.render('add.pug', {title: 'Hey', message: 'Hello there!'})
+})
 
 //Get all employees
 app.get('/employees', (req, res) => {
@@ -57,14 +47,11 @@ app.delete('/employees/:id', (req, res) => {
 //Insert an employees
 app.post('/employees', (req, res) => {
     let emp = req.body;
-    var sql = "SET @EmpID = ?;SET @Name = ?;SET @EmpCode = ?;SET @Salary = ?; \
-    CALL EmployeeAddOrEdit(@EmpID,@Name,@EmpCode,@Salary);";
-    mysqlConnection.query(sql, [emp.EmpID, emp.Name, emp.EmpCode, emp.Salary], (err, rows, fields) => {
+    const sql = `INSERT INTO Employee (Name, EmpCode, Salary)
+                 values ( ` + "'" + req.body.Name + "'" + ',' + "'" + req.body.EmpCode + "'" + ',' + "'" + req.body.Salary + "'" + ');';
+    mysqlConnection.query(sql, (err, rows, fields) => {
         if (!err)
-            rows.forEach(element => {
-                if(element.constructor == Array)
-                res.send('Inserted employee id : '+element[0].EmpID);
-            });
+            res.send(rows);
         else
             console.log(err);
     })
