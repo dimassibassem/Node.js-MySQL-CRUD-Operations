e46ea37de6543eaffd049cb09e20de89c078b4c1
Bassem Dimassi <dimassibassem99@gmail.com>
2022-01-17 22:52:13 +0100

    init

---
+++ db.js
@@ -0,0 +1,16 @@
+const mysql = require("mysql");
+var mysqlConnection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'EmployeeDB',
+    multipleStatements: true
+});
+
+mysqlConnection.connect((err) => {
+    if (!err)
+        console.log('DB connection succeded.');
+    else
+        console.log('DB connection failed \n Error : ' + JSON.stringify(err, undefined, 2));
+});
+module.exports = mysqlConnection;
